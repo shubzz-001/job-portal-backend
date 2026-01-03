d6658cae15ea0dd381ac82f61c1fc649f5d81941
shubzz-001 <sdraje01@gmail.com>
2026-01-03 13:11:02 +0530

    Initial Spring Boot setup for Job Portal System

---
+++ src/test/java/com/jobportal/Job_Portal_System/JobPortalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobportal.Job_Portal_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
